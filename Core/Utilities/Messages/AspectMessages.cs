995690f7ca04dd3aae494473179955fc4ebd85d9
Harun Karagöz <harunkaragoz1@gmail.com>
2024-10-22 11:34:23 +0300

    feat: Add Aspect Messages

---
+++ Core/Utilities/Messages/AspectMessages.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Messages
+{
+    public class AspectMessages
+    {
+        public static string WrongLoggerType = "Wrong Logger Type";
+        public static string WrongLogger = "Wrong Logger";
+    }
+}
