f8667e12f8690f481baed4e7f50898852bd15bd9
Harun Karagöz <harunkaragoz1@gmail.com>
2024-10-22 14:51:38 +0300

    feat: Add Dependency Resolvers to MVC

---
+++ WebUI/DependencyResolvers/Autofac/AutofacMVCModule.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+using System.Reflection;
+using WebUI.Services.Abstract;
+using WebUI.Services.Concrete;
+using Module = Autofac.Module;
+
+namespace WebUI.DependencyResolvers.Autofac
+{
+    public class AutofacMVCModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<BlogManager>().As<IBlogService>().SingleInstance();
+
+        }
+    }
+}
