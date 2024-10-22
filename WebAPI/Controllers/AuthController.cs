58a8a27287ab3951c0779fbc08a35f908a7edb80
Harun Karagöz <harunkaragoz1@gmail.com>
2024-10-22 12:24:00 +0300

    feat: Add Controllers

---
+++ WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,53 @@
+﻿using Business.Abstract;
+using Entity.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> LoginAsync(UserForLoginDto userForLoginDto)
+        {
+            var userToLogin = await _authService.LoginAsync(userForLoginDto);
+            if (!userToLogin.IsSuccess)
+            {
+                return BadRequest(userToLogin.Message);
+            }
+            var token = await _authService.CreateAccessTokenAsync(userToLogin.Data);
+            if (!token.IsSuccess)
+            {
+                return BadRequest(token.Message);
+            }
+            return Ok(token.Data);
+
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> RegisterAsync(UserForRegisterDto userForRegisterDto)
+        {
+            var userExists = await _authService.UserExistsAsync(userForRegisterDto.Email);
+            if (!userExists.IsSuccess)
+            {
+                return BadRequest(userExists.Message);
+            }
+            var resultOfRegister = _authService.RegisterAsync(userForRegisterDto, userForRegisterDto.Password);
+            var token = await _authService.CreateAccessTokenAsync(resultOfRegister.Result.Data);
+            if (!token.IsSuccess)
+            {
+                return BadRequest(token.Message);
+            }
+            return Ok(token.Data);
+        }
+    }
+}
---
+++ WebAPI/Controllers/BlogController.cs
@@ -0,0 +1,74 @@
+﻿using Business.Abstract;
+using Entity.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private readonly IBlogService _blogService;
+
+        public BlogController(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+
+
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _blogService.GetAllAsync();
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result.Data);
+        }
+
+
+        [HttpGet("Get/{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var result = await _blogService.GetAsync(id);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result.Data);
+        }
+
+        [HttpPost("Add")]
+        public async Task<IActionResult> Add(Blog blog)
+        {
+            var result = await _blogService.AddAsync(blog);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result.Message);
+        }
+        [HttpPut("Update")]
+        public async Task<IActionResult> Update(Blog blog)
+        {
+            var result = await _blogService.UpdateAsync(blog);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result.Message);
+        }
+        [HttpDelete("Delete/{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _blogService.DeleteAsync(id);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result);
+            }
+            return Ok(result.Message);
+        }
+    }
+}
