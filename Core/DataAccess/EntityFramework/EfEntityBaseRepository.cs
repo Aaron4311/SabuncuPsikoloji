0036a35e79f804bdb0983a3e9d41b2cbaebef30b
Harun Karagöz <harunkaragoz1@gmail.com>
2024-10-22 10:29:58 +0300

    feat: Add DataAccess files in Core Layer

---
+++ Core/DataAccess/EntityFramework/EfEntityBaseRepository.cs
@@ -0,0 +1,63 @@
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public class EfEntityBaseRepository<TEntity,TContext> 
+        where TEntity : class,IEntity,new()
+        where TContext : DbContext,new()
+    {
+        public async Task AddAsync(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task DeleteAsync(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                return await context.Set<TEntity>().FirstOrDefaultAsync(filter);
+            }
+        }
+
+        public async Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                return filter == null ? await context.Set<TEntity>().ToListAsync() : await context.Set<TEntity>().Where(filter).ToListAsync();
+            }
+        }
+
+        public async Task UpdateAsync(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                var modifiedEntity = context.Entry(entity);
+                modifiedEntity.State = EntityState.Modified;
+                await context.SaveChangesAsync();
+
+            }
+        }
+    }
+}
