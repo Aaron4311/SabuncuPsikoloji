403895da81079713773311d3c93bea03221a0457
Harun Karagöz <harunkaragoz1@gmail.com>
2024-10-22 14:51:53 +0300

    feat: Add Models to MVC

---
+++ WebUI/Models/BlogModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebUI.Models
+{
+    public class BlogModel
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public string BlogUrl { get; set; }
+
+    }
+}
