65f529dcd071ef894bf91949ea727b8af551d62c
Harun Karagöz <harunkaragoz1@gmail.com>
2024-10-22 14:52:10 +0300

    feat: Add Blog Service to MVC

---
+++ WebUI/Services/Abstract/IBlogService.cs
@@ -0,0 +1,11 @@
+﻿using WebUI.Models;
+
+namespace WebUI.Services.Abstract
+{
+    public interface IBlogService
+    {
+        Task<List<BlogResponseDto>> GetAll();
+        Task<BlogResponseDto> GetBlogByUrl(string blogUrl);
+
+    }
+}
---
+++ WebUI/Services/Concrete/BlogManager.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using WebUI.Models;
+using WebUI.Services.Abstract;
+
+namespace WebUI.Services.Concrete
+{
+    public class BlogManager : IBlogService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _baseApiUrl;
+
+        public BlogManager(HttpClient httpClient, IOptions<ApiSettings> apiSettings)
+        {
+            _httpClient = httpClient;
+            _baseApiUrl = apiSettings.Value.BaseApiUrl + "/Blog";
+        }
+
+        public async Task<List<BlogResponseDto>> GetAll()
+        {
+            var response = await _httpClient.GetAsync($"{_baseApiUrl}/GetAll");
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                var json = JsonConvert.DeserializeObject<List<BlogResponseDto>>(result);
+                return json;
+            }
+            return null;
+        }
+
+        public async Task<BlogResponseDto> GetBlogByUrl(string blogUrl)
+        {
+            var response = await _httpClient.GetAsync($"{_baseApiUrl}/GetByUrl/{blogUrl}");
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                var json = JsonConvert.DeserializeObject<BlogResponseDto>(result);
+                return json;
+            }
+            return null;
+        }
+
+    }
+}
