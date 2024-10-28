70b98ae61cf3111427d7ecd65d1c097d673e53b4
Harun Karagöz <harunkaragoz1@gmail.com>
2024-10-28 17:37:07 +0300

    style: Rework styles

--- WebUI/wwwroot/js/function.js
+++ WebUI/wwwroot/js/function.js
@@ -1,6 +1,8 @@
 (function ($) {
     "use strict";
+
 	
+
 	var $window = $(window); 
 	var $body = $('body'); 
 
---
+++ WebUI/wwwroot/js/script.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+	if (window.innerWidth > 768) { // Masaüstü için kontrol
+		document.getElementById('popup').style.display = 'block';
+		console.log("PopUp Log")
+	}
+};
+
+// Kapatma butonuna týklanýnca popup'ý kapat
+document.getElementById('closePopup').addEventListener('click', function () {
+	closePopup();
+});
+
+// Ekranýn herhangi bir yerine týklanýnca popup'ý kapat
+window.addEventListener('click', function (event) {
+	const popup = document.getElementById('popup');
+	if (event.target === popup) {
+		closePopup();
+	}
+});
+
+// Popup'ý kapatan fonksiyon
+function closePopup() {
+	document.getElementById('popup').style.display = 'none';
+}
