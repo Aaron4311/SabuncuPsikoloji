681be3484775d050ecb15f1a9325689653dedca5
Harun Karagöz <harunkaragoz1@gmail.com>
2024-10-28 17:31:55 +0300

    refactor: Refactor the classes

--- Core/Utilities/Interception/AspectInterceptorSelector.cs
+++ Core/Utilities/Interception/AspectInterceptorSelector.cs
@@ -19,9 +19,9 @@ namespace Core.Utilities.Interceptors
             var classAttributes = type.GetCustomAttributes<MethodInterceptionBaseAttribute>(true).ToList();
             var methodAttributes = type.GetMethod(method.Name).GetCustomAttributes<MethodInterceptionBaseAttribute>(true);
             classAttributes.AddRange(methodAttributes);
-            classAttributes.Add(new PerformanceAspect(3));
-            classAttributes.Add(new ExceptionAspect(typeof(FileLogger)));
-            classAttributes.Add(new LogAspect(typeof(FileLogger)));
+            //classAttributes.Add(new PerformanceAspect(3));
+            //classAttributes.Add(new ExceptionAspect(typeof(FileLogger)));
+            //classAttributes.Add(new LogAspect(typeof(FileLogger)));
 
             return classAttributes.OrderBy(x => x.Priority).ToArray();
         }
