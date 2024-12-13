5aa49c43722eba695e161a97b6c653964a785d20
Harun Karagöz <harunkaragoz1@gmail.com>
2024-12-13 14:04:28 +0300

    chore: Update inheritance

--- DataAccess/Concrete/EntityFramework/EfSliderContentDal.cs
+++ DataAccess/Concrete/EntityFramework/EfSliderContentDal.cs
@@ -1,4 +1,5 @@
 using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework.Context;
 using Entity.Concrete;
 using System;
@@ -9,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfSliderContentDal : EfEntityBaseRepository<SliderContent,SabuncuPsikolojiDbContext>
+    public class EfSliderContentDal : EfEntityBaseRepository<SliderContent,SabuncuPsikolojiDbContext> , ISliderContentDal
     {
     }
 }
