f55830e5493b6e40588432333155cb604c654f37
Harun Karagöz <harunkaragoz1@gmail.com>
2024-12-13 12:34:32 +0300

    feat: Add SliderContent Entity

---
+++ Entity/Concrete/SliderContent.cs
@@ -0,0 +1,16 @@
+using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Concrete
+{
+    public class SliderContent : IEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
