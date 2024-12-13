9d5ef160723e077938095490c42fd0438938e9d3
Harun Karagöz <harunkaragoz1@gmail.com>
2024-12-13 12:37:24 +0300

    feat: Add ImageUrl Property To SliderContent Entity

--- Entity/Concrete/SliderContent.cs
+++ Entity/Concrete/SliderContent.cs
@@ -12,5 +12,6 @@ namespace Entity.Concrete
         public int Id { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
