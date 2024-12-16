d5126857062d10b3000bd01c0191488d2b1ac421
Harun Karagöz <harunkaragoz1@gmail.com>
2024-12-16 09:26:17 +0300

    feat: Add DAL classes for just added Entities

---
+++ DataAccess/Concrete/EntityFramework/EfCommentDal.cs
@@ -0,0 +1,16 @@
+using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Context;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCommentDal : EfEntityBaseRepository<Comment,SabuncuPsikolojiDbContext> , ICommentDal
+    {
+    }
+}
